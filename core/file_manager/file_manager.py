9094408bec51ec9a98426cc14b311f41f56a253d
lanzero <615798102@qq.com>
2016-11-06 17:32:18 +0800

    define the interfaces of the file_manager and raid_manager

---
+++ core/file_manager/file_manager.py
@@ -0,0 +1,60 @@
+#coding: utf-8
+
+
+class manager:
+# 文件管理系统，此系统是整个系统的文件管理层，向下与raid_manager直接相连，中间提供了一个概念上的文件树的管理
+    # 文件目录
+    list = []
+    # 回收站机制
+    trash_bin = False
+    # 文件查看
+    def list(self, path = '/'):
+        pass
+        #由recloud模块来调用list（）方法，获取文件目录
+        #由根目录下来获得，字典，Jason文件，交由系统同步
+    #系统文件操作
+    def sys_operation(self, device_name, op_code):
+        pass
+        # note 针对节点而言
+        # 相应的设备目录下的系统配置文件操作
+        # 下载配置文件的时候无文件映射表，需要自动恢复相应的文件
+
+    def fetch_devices(self):
+        pass
+        #note 针对节点而言
+        #返回sys目录下的所有的子目录的名字
+    # 上传
+    def upload(self, path = '/', name):
+        pass
+        #检查操作
+        #调用相应的raid_manager方法来上传文件
+        #得到上传成功的回应，然后修改当前文件树
+        #如果出错，执行异常处理
+    # 下载
+    def download(self, path, name):
+        pass
+        #调用相应的raid_manager方法来下载文件
+        #检查文件的完整性
+
+    # 删除文件
+    def discard(self, self.trash_bin):
+        pass
+        # 调用相应的raid_manager方法来删除
+        # 修改当前的文件树
+    #返回信息
+    def info(self):
+        pass
+        # 已用容量
+        # 剩余容量
+        # 总容量信息
+    #记录操作信息
+    def __log__(self):
+        pass
+        #记录操作信息
+        #操作类型，操作时间，完成时间，操作者
+        #
+
+    def  exist(self, path = '/', )
+
+if __name__ == '__main__':
+    #instance
---
+++ core/raid_manager/raid_manager.py
@@ -0,0 +1,63 @@
+#encoding: utf-8
+
+#更新
+def update():
+    # 每当进行了文件操作或者查询操作的时候
+    # 需要对系统的configuration文件进行操作
+#此处为用于file_manager调用的接口
+class controller:
+    # 文件映射表
+    # MD5码 | 分块信息（adaptor序号 - 块序号 - 块路径的键值对） | 修改时间 | 原文件大小 | 冗余比例 |
+    #
+    map = []
+    recent_delete = []
+    def __init__(self):
+        # 检查系统运行根目录下是否存在文件块映射表 是：正常运行； 否： 创建
+        # 登陆节点，检查是否有名为sys的目录，有则说明为之前的节点，没有则新建一个目录，并在该sys目录下新建一个以该设备名命名的文件夹
+        # 检查sys目录下是否存在以该设备名命名的目录，如果有，则证明该设备之前曾经使用过
+        # 检查        部分不用考虑
+        def rsys_operation(self, device_name, op_code):
+            pass
+            # note 针对节点而言
+            # 相应的设备目录下的系统配置文件操作
+            # 下载配置文件的时候无文件映射表，需要有一定概率能在单一节点上恢复相应的文件，登录设备越多，概率越大
+            # 无文件映射表的机制，仅针对配置文件，1. 系统文件冗余和其他文件相分离，2. 系统文件无文件映射机制
+
+
+        def rfetch_devices(self):
+            pass
+            # note 针对节点而言
+            # 返回sys目录下的所有的子目录的名字
+    # 上传
+    def rupload(self, name, priority = False):
+        pass
+        # 将分块文件上传到adapter之中
+        # 获取到成功的反馈
+        # 成功则将信息写入映射表
+        # 优先级（priority）用于上传系统配置文件，高优先级传输时，提高冗余度
+    # 下载
+    def rdownload(self, name, priority = False):
+        pass
+        # 通过参数来查询相应的映射表
+        # 获取adaptor数组
+        # 从数组中取出相应数量的文件块
+        # 组合成文件然后返回给file_manager模块
+
+    # 查询信息
+    def get_info(self):
+        pass
+
+    # 删除文件
+    def rdiscard(self, path = '/', name, trash_bin):
+        pass
+        # 通过参数查询映射表
+        # 检查是否开启回收站机制
+        # 如果没有删除所有adapter上的文件块，并删除映射表记录
+        # 如果开启了，则将相应的记录移动到recent_discarded的字典中
+
+    # 查错
+    def check(self):
+        pass
+
+if __name__ == '__main__':
+    #instance
